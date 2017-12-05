84a9dc7dd0401e4dec948f1f0a79e985046de3c0
Jack Henahan <jack.henahan@uvmhealth.org>
2017-12-05 11:41:02 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,10 @@
+(let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
+      (bootstrap-version 2))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
