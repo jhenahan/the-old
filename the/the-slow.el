30fd324cbd1d04c6ee30b6f0d80602a550e4fed8
Jack Henahan <jack.henahan@uvmhealth.org>
2017-12-06 15:01:23 -0500

    Minor mode for slow stuff

---
+++ the/the-slow.el
@@ -0,0 +1,15 @@
+
+;;; the-slow.el --- For when Emacs just isn't fast enough
+
+(define-minor-mode the-slow-indent-mode
+  "Minor mode for when the indentation code is slow.
+This prevents `aggressive-indent' from indenting as frequently.")
+
+(define-minor-mode the-slow-autocomplete-mode
+  "Minor mode for when the autocompletion code is slow.
+This prevents `company' and `eldoc' from displaying metadata as
+quickly.")
+
+(provide 'the-slow)
+
+;;; the-slow.el ends here
