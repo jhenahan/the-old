d9f9694a42320d5764ec7bd6765823c645dff692
Jack Henahan <jack.henahan@uvmhealth.org>
2017-12-14 14:47:15 -0500

    Various config file niceties
    Also force Emacs to recognize most Ansible files as YAML, even without
    the file extension.

---
+++ the/the-regexp.el
@@ -0,0 +1,10 @@
+;;; the-regexp.el --- Support for regular expressions
+
+(require 'the-package)
+
+;; Define your regular expressions without writing regex.
+(use-package rx)
+
+(provide 'the-regexp)
+
+;;; the-regexp.el ends here
