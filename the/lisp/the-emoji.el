c357694e990179b3312e7f53d7ac08b90b067204
Jack Henahan <jack.henahan@uvmhealth.org>
2018-01-12 13:17:33 -0500

    Emojis 💟

---
+++ the/lisp/the-emoji.el
@@ -0,0 +1,11 @@
+;;; the-emoji.el --- Emojis :smile:
+
+(require 'the-package)
+
+(use-package emojify
+  :init
+  (add-hook 'after-init-hook #'global-emojify-mode))
+
+(provide 'the-emoji)
+
+;;; the-emoji.el ends here
