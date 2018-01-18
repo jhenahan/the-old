597bb45ec9dd3abe7cf6055b99ceacb47e5bc8af
Jack Henahan <jack.henahan@uvmhealth.org>
2018-01-18 14:25:42 -0500

    Shackle setup

--- the/lisp/the-windows.el
+++ the/lisp/the-windows.el
@@ -6,6 +6,17 @@
   :config
   (golden-ratio-mode t))
 
+(use-package shackle
+  :demand t
+  :config
+  (setq shackle-rules '(
+                        (compilation-mode :noselect t)
+                        (magit-diff-mode :noselect t)
+                        ("*Warnings*" :noselect t)
+                        )
+        shackle-default-rule '(:select t))
+  (shackle-mode 1))
+
 (provide 'the-windows)
 
 ;;; the-windows.el ends here
